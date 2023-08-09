2bb5e51688383a9b69dc58d167269c5ac1f398a8
Chukwubuike Kingsley <chukwubuikekingsley369@gmail.com>
2023-08-09 08:31:52 +0100

    upload

--- src/home.jsx
+++ src/home.jsx
@@ -1,10 +1,11 @@
 import React ,{useState}from 'react'
 import "./style/home.css"
-import backgroundImageDiv from "./img/imgBack.jpg"
+import backgroundImageDiv from "./img/bs1.jpg"
 import back1 from "./img/machine.jpg"
 import axios from "axios"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faFacebook,faTwitter,faLinkedin,faTelegram,faYoutube,}from '@fortawesome/free-brands-svg-icons'
+import {faPhone}from '@fortawesome/free-solid-svg-icons'
 import validator from 'validator';
 
 const Home = ()=> {
@@ -15,7 +16,8 @@ const Home = ()=> {
     email:"",
     residentialAddress:"",
     interest:"",
-    city:''
+    city:'',
+    localGovernment:""
 
   })
   const [check,setCheck]=useState(false)
@@ -41,13 +43,15 @@ const handleChange = (event) => {
 
   const postInfo =(e)=>{
 e.preventDefault()
-    if((inputs.firstName.length === 0) || (inputs.lastName.length === 0) || (inputs.mobileNumber.length === 0) || (inputs.interest.length === 0) || (inputs.address.length === 0) || (inputs.email.length === 0)){
+    if((inputs.name.length === 0) || (inputs.localGovernment.length === 0) || (inputs.mobileNumber.length === 0) || (inputs.interest.length === 0) || (inputs.residentialAddress.length === 0) || (inputs.email.length === 0) || (inputs.localGovernment.length === 0)){
       console.log("field must not be empty")
     }else if(validator.isEmail(inputs.email) === false){
 console.log("invalid Email")
+    }else if (validator.isMobilePhone(inputs.mobileNumber,'en-NG',{strictMode:false}) === false){
+ console.log("invalid phone number")
     }else{
 
-      axios.post("http://localhost:8080/api/post/form",inputs).then((response)=>{
+      axios.post("https://tsyef.onrender.com/api/post/form",inputs).then((response)=>{
         setMessage("Successfully submitted Your form")
       }).catch((err)=>{
        console.log(err)
@@ -66,18 +70,22 @@ console.log("invalid Email")
         <p className="LogoSubT">Empowering the future</p>
         </a>
         </div>
+        <div>
+        <div className='phoneDiv'><FontAwesomeIcon icon={faPhone} className="colorJoin" /> <span className='contact'>+234 {" "}8027-953-016</span></div></div>
         </div>
 
       </div>
 
-    <div style={{backgroundImage:`url(${backgroundImageDiv})`, backgroundRepeat:"no-repeat",  backgroundSize: "100% 100%",width:"100%"}} className='mainDivH'>
+    <div style={{backgroundImage:`url(${backgroundImageDiv})`, backgroundRepeat:"no-repeat",  backgroundSize: "100% 100%",width:"100%",position:'relative'}} className='mainDivH'>
+      
+      
       <div className='pip'>
       <div className="pathFile">
 <div className="heroText">
 
   <div  className='widthPar'>
     <p className="heroTextY">Empowering Youths for Success and Sustainability</p>
-     <p className="investIn">Facilitate Skill Development and Access to Machinery and Equipment</p>
+     <p className="investIn">Facilitate Skill Development and Access to  Finance and  Machinery</p>
   </div>
 
     <div className='widthPar'>
@@ -88,7 +96,7 @@ console.log("invalid Email")
             <div className="regDiv">Registration</div>
         </div>
         <label className="labelName">Name</label>
-        <div><input type="text" placeholder="name" name="name" className='inputCheck' onChange={changeHandle}/></div>
+        <div><input type="text" placeholder="full name" name="name" className='inputCheck' onChange={changeHandle}/></div>
        
          <label className="labelName">Mobile Number</label>
         <div><input type="number" placeholder="mobile number" name="mobileNumber" className='inputCheck' onChange={changeHandle} style={{color:"#000"}} min="0" max={5}/></div>
@@ -99,7 +107,9 @@ console.log("invalid Email")
         <div><input type="text" placeholder="address" name="residentialAddress"  className='inputCheck' onChange={changeHandle} style={{color:"#000"}}/></div>
         <label className="labelName">City</label>
         <div><input type="text" placeholder="city" name ="city" className='inputCheck' onChange={changeHandle}/></div>
-         <label className="labelName">State</label>
+         <label className="labelName">Local Government Of Origin</label>
+        <div><input type="text" placeholder="local government" name ="localGovernment" className='inputCheck' onChange={changeHandle}/></div>
+         <label className="labelName">State of origin</label>
         <div><input type="text" placeholder="state" name ="state" className='inputCheck' onChange={changeHandle}/></div>
         <label className="labelName">Interest</label>
         <div className="filesDiv">
@@ -121,7 +131,7 @@ console.log("invalid Email")
         <div>
           <input type="checkbox" name="agreement"  onChange={handleChange}/>
           <label  htmlFor="agree" className="agree">
-            By clicking here I give my consent for <span></span>TSYEF to contact me for marketing purposes. You can opt out at any time. For further details please see our Marketing and Communication Policy Statement.
+            By clicking here I give my consent for <span></span>TSYEF to contact me for financial purposes. You can opt out at any time. For further details please call our customer number  at top.
           </label>
         </div>
 
@@ -134,13 +144,14 @@ console.log("invalid Email")
         </div>
         </div>
         </div>
+       
 
         </div>
 
         
          <div style={{backgroundImage:`url(${back1})`, backgroundRepeat:"no-repeat",  backgroundSize: "100% 100%",width:"100%",height:"450px",position:'relative'}} className="uma">
           <div className="ppLink"></div>
-                    <div style={{position:"absolute",left:'0',right:"0",top:"0",bottom:"0"}}className='pinkL'>
+                    <div  className='pinkL'>
           <div className="pathFile" >
           <div style={{}}>
               <p className="OBjy">Objective of TSYEF</p>
@@ -151,14 +162,14 @@ console.log("invalid Email")
                   <p className="tap">Offer comprehensive training programs and workshops to enhance the skill sets of aspiring young entrepreneurs, empowering them with practical knowledge and expertise.</p> 
                 </div>
                 <div className="skillDiv">
-               <p className="facility">Provide Access to Machinery and Equipment</p>
-               <p className="tap"> Establish partnerships and collaborations with industry leaders to make advanced machinery and equipment accessible to our beneficiaries, reducing barriers to entry for their ventures</p>
+               <p className="facility">Provide Access to Machinery and Finance</p>
+               <p className="tap"> Establish partnerships and collaborations with industry leaders to make advanced machinery and Finance accessible to our beneficiaries, reducing barriers to entry for their ventures</p>
                 </div>
               </div>
                 <div  className="OBJDiv">
                 <div  className="skillDiv">
                   <p className="facility">Foster Mentorship and Guidance</p>
-                  <p className="tap" >Pair youths with experienced mentors who can offer valuable insights, guidance, and support throughout their entrepreneurial journey, aiding them in overcoming challenges and seizing opportunities.</p> 
+                  <p className="tap" >Pair selfless youths with experienced mentors who can offer valuable insights, guidance, and support throughout their entrepreneurial journey, aiding them in overcoming challenges and seizing opportunities.</p> 
                 </div>
                 <div  className="skillDiv">
                <p className="facility">Promote Talent Recognition</p>
@@ -182,15 +193,15 @@ console.log("invalid Email")
                 <li><a href=""><FontAwesomeIcon icon={faTelegram} className="iconSize" /></a></li>
               </ul>
               </div>
-            <p className="pli">TSYEF (Today's Selfless Youths Empowerment Foundation) is a dynamic and socially driven enterprise committed to empowering youths, aged 18 to 45 years, to pursue and sustain their goals successfully. Our primary objective is to equip young individuals with essential skills, cutting-edge machinery, and state-of-the-art equipment to ensure their journey towards self-reliance and prosperity.</p>
+            <p className="pli">TSYEF (Today's Selfless Youths Empowerment Foundation) is a dynamic and socially driven enterprise committed to empowering youths, aged 18 to 45 years, to pursue and sustain their goals successfully. Our primary objective is to equip young individuals with essential Finance,skills, cutting-edge machinery, and state-of-the-art equipment to ensure their journey towards self-reliance and prosperity.</p>
             {/* <p>Our primary target market includes youths aged 18 to 45 years with an entrepreneurial spirit, a passion for creativity, and a desire to achieve self-reliance. We will focus on both urban and rural areas, ensuring inclusivity and equal opportunities for all.</p> */}
           <p className="pli">
 
-We believe that empowering youths with skills, machinery, and opportunities is the key to unlocking their potential and creating a brighter future for all. By focusing on sustainable development, mentorship, and talent recognition, we are committed to being a catalyst for positive change and fostering a new generation of successful and socially responsible entrepreneurs.
+We believe that empowering  selfless youths with Finance, skills, machinery, and opportunities is the key to unlocking their potential and creating a brighter future for all. By focusing on sustainable development, mentorship, and talent recognition, we are committed to being a catalyst for positive change and fostering a new generation of successful and socially responsible entrepreneurs.
 
 
 </p>
-<p  className="pli">Join us on this transformative journey to make a lasting impact on the lives of youths and the communities they serve. Together, we can empower dreams and build a better tomorrow.
+<p  className="pli">Join us on this transformative journey to make a lasting impact on the lives of selfless youths and the communities they serve. Together, we can empower dreams and build a better tomorrow.
 </p>
 <p className="copyRight">©  2023  TSYEF</p>
           </div>
